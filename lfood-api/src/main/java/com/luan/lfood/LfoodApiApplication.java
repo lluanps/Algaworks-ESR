5bccf6a4ce0cad812037d2168e5248cf481d4b4c
lluanps <lluanps@gmail.com>
2023-04-27 17:17:29 -0300

    Projeto criado

---
+++ lfood-api/src/main/java/com/luan/lfood/LfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.luan.lfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfoodApiApplication.class, args);
+	}
+
+}
